de115ea03a5e42efd03b3e1075b89aca4845be26
developython14 <djmel1955@gmail.com>
2022-12-22 06:12:23 +0100

    try to connect django wih react

--- frontend/src/componant/header.js
+++ frontend/src/componant/header.js
@@ -1,11 +1,38 @@
 
 import React  ,{Component} from 'react';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Card,CardBody,CardTitle,CardSubtitle,CardText,Button } from 'reactstrap';
 
 
 class Header extends Component {
     render() {
-      return <h1>hada my first app with react and django for beatrice</h1>
+      return <Card
+      style={{
+        width: '18rem'
+      }}
+    >
+      <img
+        alt="Sample"
+        src="https://picsum.photos/300/200"
+      />
+      <CardBody>
+        <CardTitle tag="h5">
+          Card title
+        </CardTitle>
+        <CardSubtitle
+          className="mb-2 text-muted"
+          tag="h6"
+        >
+          Card subtitle
+        </CardSubtitle>
+        <CardText>
+          Some quick example text to build on the card title and make up the bulk of the card‘s content.
+        </CardText>
+        <Button>
+          Button
+        </Button>
+      </CardBody>
+    </Card>
     }
   }
 

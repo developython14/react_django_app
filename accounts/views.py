93c243200b10e30864399da26dfa59545f0f9c90
developython14 <djmel1955@gmail.com>
2022-12-22 04:39:54 +0100

    ad

--- accounts/views.py
+++ accounts/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 
 
 def dash(request):
-    return render(request, 'build/home.html')
+    return render(request, 'index.html')
--- inr/settings.py
+++ inr/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'inr.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [  os.path.join(BASE_DIR, 'react'), os.path.join(BASE_DIR, 'templates') ],
+        'DIRS': [  os.path.join(BASE_DIR, 'react/build'), os.path.join(BASE_DIR, 'templates') ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

b78e2926464d05520489dc3009a90b01a3a4f129
developython14 <djmel1955@gmail.com>
2022-12-25 03:57:43 +0100

    customize the user model

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.contrib.auth.forms import AuthenticationForm  
 from .admin import UserCreationForm,UserChangeForm
 # Create your views here.
-from django.views.decorators.csrf import csrf_protect
 
 
 def dash(request):
@@ -16,6 +15,9 @@ def login(request):
 def signup(request):
     return render(request, 'signup.html')
 
-@csrf_protect
 def test(request):
+    if request.method == 'POST' :
+        form = UserCreationForm(request.POST)
+        print('hadi  recived data')
+        print(form.cleaned_data)
     return render(request, 'test.html' , {'form':UserCreationForm})

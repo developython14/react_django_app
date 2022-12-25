a837c0a7d6f5757ddfba1e3a01cd9da0af943197
developython14 <djmel1955@gmail.com>
2022-12-25 19:05:18 +0100

    customize the user model

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,9 +2,17 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import Person
+from django.contrib.auth.models import User
 
 class CustomUserCreationForm(UserCreationForm):
 
     class Meta:
         model = Person
         fields = ("saroukh",)
+
+
+class basecreate(forms.ModelForm):
+
+    class Meta:
+        model = User
+        fields = ('first_name' , 'last_name' , 'email' , 'username')
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import AuthenticationForm  ,UserCreationForm,UserChangeForm
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm,basecreate
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
+from twilio.rest import Client
 
 # Create your views here.
 
@@ -21,6 +22,16 @@ def login(request):
     html_message = render_to_string('test.html' ,data),
     fail_silently=False,
     )
+    account_sid = 'AC5429cafece758cdb12fc1119fb74b7d6'
+    auth_token = '2f982e0e703de72f674a79d9c3aa70a2'
+    client = Client(account_sid, auth_token)
+
+    message = client.messages \
+                .create(
+                     body="Join Earth's mightiest heroes. Like Kevin Bacon.",
+                     from_='+13854692429',
+                     to='+213674901699'
+                 )
     return render(request, 'login.html' , {'form':AuthenticationForm})
 
 
@@ -33,4 +44,4 @@ def test(request):
         if data_form.is_valid():
             print('data rahi  validated')
             data_form.save()
-    return render(request, 'test.html' , {'form':CustomUserCreationForm})
+    return render(request, 'test.html' , {'form':CustomUserCreationForm ,'form1':basecreate})

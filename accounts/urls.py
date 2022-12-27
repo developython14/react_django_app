4b2a125bc989ab22334f7f41a46b29494ee4da9e
developython14 <djmel1955@gmail.com>
2022-12-27 18:15:51 +0100

    createing room and web socket io

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('dash', views.dash, name='dash'),
     path('logout', views.logoutview, name='logout'),
     path('room', views.main_view, name='room'),
+    path('activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/',  
+        views.activate, name='activate'),  
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,HttpResponse
 from django.contrib.auth.forms import AuthenticationForm  ,UserCreationForm,UserChangeForm
 from .forms import CustomUserCreationForm,basecreate
 from django.core.mail import send_mail
@@ -9,12 +9,13 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login ,logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.sites.shortcuts import get_current_site  
-from django.utils.encoding import force_bytes, force_text  
+from django.utils.encoding import force_bytes, force_str  
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode  
 from django.template.loader import render_to_string  
 from .tokens import account_activation_token  
 from django.contrib.auth.models import User  
 from django.core.mail import EmailMessage  
+from django.contrib.auth import get_user_model
 
 
 # Create your views here.
@@ -53,6 +54,18 @@ def signup(request):
         print('hadi password' , password)
         user.set_password(password)
         user.save()
+        current_site = get_current_site(request)  
+        mail_subject = 'Activation link has been sent to your email id'  
+        message = render_to_string('email_confirm.html', {  
+                'user': user,  
+                'domain': current_site.domain,  
+                'uid':urlsafe_base64_encode(force_bytes(user.pk)),  
+                'token':account_activation_token.make_token(user),  
+            })  
+        mail = EmailMessage(  
+                        mail_subject, message, to=[email]  
+            )  
+        mail.send() 
         university = request.POST['university']
         accept_contrat = True
         print('hadi hya la valeur dyale' ,accept_contrat )
@@ -72,7 +85,7 @@ def test(request):
         user.set_password(password)
         current_site = get_current_site(request)  
         mail_subject = 'Activation link has been sent to your email id'  
-        message = render_to_string('acc_active_email.html', {  
+        message = render_to_string('email_confirm.html', {  
                 'user': user,  
                 'domain': current_site.domain,  
                 'uid':urlsafe_base64_encode(force_bytes(user.pk)),  
@@ -94,3 +107,17 @@ def test(request):
 def main_view(request):
     context = {}
     return render(request, 'room.html', context=context)
+
+def activate(request, uidb64, token):  
+    User = get_user_model()  
+    try:  
+        uid = force_text(urlsafe_base64_decode(uidb64))  
+        user = User.objects.get(pk=uid)  
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):  
+        user = None  
+    if user is not None and account_activation_token.check_token(user, token):  
+        user.is_active = True  
+        user.save()  
+        return HttpResponse('Thank you for your email confirmation. Now you can login your account.')  
+    else:  
+        return HttpResponse('Activation link is invalid!')  

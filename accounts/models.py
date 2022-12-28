7d93509b81f3fdfa9acaac62e1579d95364ca5cf
developython14 <djmel1955@gmail.com>
2022-12-28 04:48:30 +0100

    igre

--- accounts/models.py
+++ accounts/models.py
@@ -8,10 +8,23 @@ User._meta.get_field('email')._unique = True
 
 
 
-class Person(models.Model):
-    university = models.CharField(max_length=30)
-    accept_contrat = models.BooleanField()
-    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+class Countries(models.Model):
+    name = models.CharField(max_length=30 ,unique=True)
     USERNAME_FIELD = 'username'
     
+class Organismes(models.Model):
+    name = models.CharField(max_length=30 ,unique=True)
+    website = models.CharField(max_length=30 ,unique=True)
+    adresse = models.CharField(max_length=30 ,unique=True)
+    USERNAME_FIELD = 'username'
+
+class Person(models.Model):
+    organisme = models.ForeignKey(to=Organismes , on_delete=models.CASCADE)
+    accept_contrat = models.BooleanField()
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    country = models.ForeignKey(to=Countries, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=30 ,unique=True)
+    emploi = models.CharField(max_length=30 ,unique=True)
+    USERNAME_FIELD = 'username'
     
+

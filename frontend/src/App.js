fbcae4eee17d6220f7601973ba882aa75bc0ca56
developython14 <djmel1955@gmail.com>
2022-12-31 04:51:04 +0100

    update moedls and chat systems

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import {Example} from './componant/header.js';
+import {Navbar} from './componant/navbar.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Example></Example>
-        <p>salam</p>
+        <Navbar></Navbar>
       </header>
     </div>
   );
---
+++ frontend/src/pages/shared_componant/navbar.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState,Component } from 'react';
+
+
+class Navbar extends Component {
+    render() {
+      return <h1>Hello, belkassam Mustapha</h1>;
+    }
+  }
+
+
+
+  export {Navbar};

596c27edec270f1ec940778a331416be057379b5
developython14 <djmel1955@gmail.com>
2022-12-31 04:41:01 +0100

    update moedls and chat systems

--- inr/settings.py
+++ inr/settings.py
@@ -152,8 +152,11 @@ ASGI_APPLICATION = 'inr.asgi.application'
 
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer"
-    }
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 8000)],
+        },
+    },
 }
 
 

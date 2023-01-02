dc18889d3d8333033e315c91aa44a40a9349f69e
developython14 <djmel1955@gmail.com>
2023-01-02 04:20:06 +0100

    work on react parts

--- frontend/src/pages/chat/chat.js
+++ frontend/src/pages/chat/chat.js
@@ -7,6 +7,7 @@ import { Offcanvas
 
 import _Navbar from '../shared_componant/navbar';
 import TextField from '@mui/material/TextField';
+import Avatar from '@mui/material/Avatar';
 
 
 function Chat (){
@@ -16,8 +17,11 @@ function Chat (){
             <div className='section1'>
                 <h3>Message</h3>
                 <p>223 Messages , 2 unread</p>
-                <TextField id="standard-basic" label="Search" variant="Search" />
-                <message_componant></message_componant>
+                <TextField id="outlined-basic" label="Search" variant="outlined" />
+                <div className='d-flex flex-column'>
+                <Message_componant></Message_componant>
+
+                </div>
             </div>
             <div className='section2'></div>
             
@@ -29,9 +33,16 @@ function Chat (){
 
 
 
-function message_componant(){
+function Message_componant(){
     return(
-        <p>free allo</p>
+        <div className='d-flex flex-row col-7 bg-light p-3 align-items-start'>
+        <Avatar alt="Remy Sharp" src="https://media.licdn.com/dms/image/C4E03AQG5Jvo_fUP0Sg/profile-displayphoto-shrink_800_800/0/1629473101419?e=1678320000&v=beta&t=2qzS8vDum0nhhuBwRaAgVQbrYV6RyRc3X0FarZSZs3g" />
+        <div className='d-flex flex-column'>
+            <h6>Mustapha Belkassam</h6>
+            <p>loca FormatListNumberedRtl fwefwwer fsd sfsfsdfsd  fdsaf </p>
+        </div>
+        <p>30 m</p>
+        </div>
     )
 }
 

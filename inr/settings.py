4cae3274fd7d0ef9dac75685bda694c4e80619bc
developython14 <djmel1955@gmail.com>
2023-01-02 17:52:53 +0100

    work on react parts

--- inr/settings.py
+++ inr/settings.py
@@ -152,10 +152,7 @@ ASGI_APPLICATION = 'inr.asgi.application'
 
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [("127.0.0.1", 8000)],
-        },
+        "BACKEND": "channels.layers.InMemoryChannelLayer"
     },
 }
 

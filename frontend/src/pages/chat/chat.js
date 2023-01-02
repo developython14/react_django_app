901b8a0c8670ba8a22d9b3a7edeec71b54947825
developython14 <djmel1955@gmail.com>
2023-01-02 04:00:06 +0100

    work on react parts

--- frontend/src/pages/chat/chat.js
+++ frontend/src/pages/chat/chat.js
@@ -6,13 +6,20 @@ import { Offcanvas
 } from 'reactstrap';
 
 import _Navbar from '../shared_componant/navbar';
+import TextField from '@mui/material/TextField';
 
 
 function Chat (){
     
     return (
-        <div>
-        <p>hadi chat page</p>
+        <div className='d-flex flex-row'>
+            <div className='section1'>
+                <h3>Message</h3>
+                <p>223 Messages , 2 unread</p>
+                <TextField id="standard-basic" label="Standard" variant="standard" />
+            </div>
+            <div className='section2'></div>
+            
         </div>
   
     )

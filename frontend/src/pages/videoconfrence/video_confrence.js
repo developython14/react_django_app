e9a3a10ca054f0208598af827bbe29f8251713c5
developython14 <djmel1955@gmail.com>
2023-01-09 08:00:27 +0100

    work on videoconfrence chat parts

--- frontend/src/pages/videoconfrence/video_confrence.js
+++ frontend/src/pages/videoconfrence/video_confrence.js
@@ -40,9 +40,25 @@ function Video_stream (){
         <source src="http://techslides.com/demos/sample-videos/small.mp4"
                 type="video/mp4" /> 
         </video>
+        <video autoPlay = {true} controls height={'100px'} width = {'200px'}>
+        <source src="http://techslides.com/demos/sample-videos/small.mp4"
+                type="video/mp4" /> 
+        </video>
+        <video autoPlay = {true} controls height={'100px'} width = {'200px'}>
+        <source src="http://techslides.com/demos/sample-videos/small.mp4"
+                type="video/mp4" /> 
+        </video>
+        <video autoPlay = {true} controls height={'100px'} width = {'200px'}>
+        <source src="http://techslides.com/demos/sample-videos/small.mp4"
+                type="video/mp4" /> 
+        </video>
+        <video autoPlay = {true} controls height={'100px'} width = {'200px'}>
+        <source src="http://techslides.com/demos/sample-videos/small.mp4"
+                type="video/mp4" /> 
+        </video>
         </div>
 
-        <div className='container d-flex flex-row justify-content-between align-items-center'>
+        <div className='container d-flex flex-row justify-content-between align-items-center border border-primary'>
           <div className='d-flex flex-row justify-content-center'>
             <p>01:15</p>
             <p>fsdfas-fdsdfas</p>
